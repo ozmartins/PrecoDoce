b19bd3d3f95b4f57359aa193cbc9114e01514ca4
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-09-20 19:06:14 -0300

    Cria view index em todas as apps e cria um template basico inicial para todas as apps.

--- preco_doce/urls.py
+++ preco_doce/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = [    
+    path('', include('dashboard.urls')),
+    path('calculos/', include('calculos.urls')),
+    path('compras/', include('compras.urls')),
+    path('receitas/', include('receitas.urls')),
+    path('admin/', admin.site.urls)
 ]
