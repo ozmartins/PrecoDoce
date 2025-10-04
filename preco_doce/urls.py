efd5aaed9485945eb115933791610bd355da4945
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-10-04 19:42:28 -0300

    feat(dashboard): change initial page visual to a pink theme

--- preco_doce/urls.py
+++ preco_doce/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [    s
+urlpatterns = [    
+    path('', include('dashboard.urls')),
+    #path('calculos/', include('calculos.urls')),
+    #path('compras/', include('compras.urls')),
+    #path('receitas/', include('receitas.urls')),
     path('admin/', admin.site.urls)
 ]
